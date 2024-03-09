65133f7106f38bfe2b5142ca84a6950f06725750
NNakreSS <serkanatmaca15@gmail.com>
2024-03-09 19:11:42 +0300

    create footer add social icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 // pages
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => (
   <div className="dark dark:bg-zinc-900  min-h-screen min-w-full dark:text-gray-200 p-5 grid grid-cols-12 gap-4">
@@ -12,6 +13,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   </div>
 );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,61 @@
+import { FiMail, FiMapPin } from "react-icons/fi";
+import {
+  FaGithub,
+  FaYoutube,
+  FaInstagramSquare,
+  FaShoppingCart,
+} from "react-icons/fa";
+
+const Footer = () => (
+  <footer className="border-zinc-800 border-t-2 mt-20 text-white py-8">
+    <div className="container mx-auto flex flex-col lg:flex-row justify-between px-4">
+      <div className="flex flex-col mb-4 lg:mb-0">
+        <h5 className="text-lg font-bold mb-2">İletişim Bilgileri</h5>
+        <div className="flex items-center mb-2">
+          <FiMapPin className="mr-2" />
+          <p>Gaziantep, Türkiye</p>
+        </div>
+        <div className="flex items-center">
+          <FiMail className="mr-2" />
+          <p>nakresdevelopment@gmail.com</p>
+        </div>
+      </div>
+      <div className="flex flex-row gap-3 text-5xl">
+        <a
+          href="https://github.com/NNakreSS"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-blue-400 hover:scale-110 duration-300"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCkHHGxb-5Dun5xcBRZM9CEQ"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-red-400 hover:scale-110 duration-300"
+        >
+          <FaYoutube />
+        </a>
+        <a
+          href="https://www.instagram.com/serkanb.a/"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-pink-500 hover:scale-110 duration-300"
+        >
+          <FaInstagramSquare />
+        </a>
+        <a
+          href="https://nakres-development.tebex.io/"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-blue-400 hover:scale-110 duration-300"
+        >
+          <FaShoppingCart />
+        </a>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
