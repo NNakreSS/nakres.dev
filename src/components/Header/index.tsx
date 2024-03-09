dd33870780eb41d8cd0cd564e154999d1956adf6
NNakreSS <serkanatmaca15@gmail.com>
2024-03-09 13:20:42 +0300

    add typing animation and smaller web

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,14 +7,14 @@ import lightIcon from "../../assets/icons/lightIcon.svg";
 
 // NavLink check active className
 const isNavActive = ({ isActive }: { isActive: boolean }): string =>
-  isActive ? "bg-zinc-800/50 p-3 rounded-xl" : "p-3";
+  isActive ? "bg-zinc-800/50 p-3 rounded-xl text-amber-400" : "p-3";
 
 const Header = () => (
-  <header className="flex items-center justify-between h-24 w-full border-b-2 border-zinc-800">
+  <header className="flex items-center justify-between h-16 w-full border-b-2 border-zinc-800">
     <div id="logo">
-      <img src={white_logo} alt="NakreS development logo" />
+      <img src={white_logo} alt="NakreS development logo" className="w-14" />
     </div>
-    <nav className="flex gap-5 items-center justify-between font-bold text-xl">
+    <nav className="flex gap-5 items-center justify-between font-bold text-md">
       <NavLink to="/" className={isNavActive}>
         Anasayfa
       </NavLink>
@@ -29,7 +29,7 @@ const Header = () => (
       </NavLink>
     </nav>
     <div id="dark-mode-toggle">
-      <img src={lightIcon} alt="Dark mode moon/sun icon" />
+      <img src={lightIcon} alt="Dark mode moon/sun icon" className="w-14" />
     </div>
   </header>
 );
--- src/pages/Home/CodeBlock.tsx
+++ src/pages/Home/CodeBlock.tsx
@@ -1,23 +1,64 @@
-const CodeBlock = () => (
-  <div className="col-span-5 bg-zinc-800 rounded-md ">
-    <div
-      id="code-head"
-      className="h-10 w-full bg-zinc-700 rounded-t-md flex items-center cursor-pointer"
-    >
-      <div className="flex items-center justify-between h-2/4 w-3/12 ml-1 bg-zinc-900 p-4 rounded-sm">
-        <span className="font-bold text-yellow-400">JS</span>{" "}
-        <span>Hello.js</span> <span className="font-thin text-gray-200">x</span>
+import { useEffect, useState } from "react";
+
+const CodeBlock = () => {
+  const areasOfExpertise: string[] = ["FrontEnd", "BackEnd", "Mobile", "Game"];
+  const [indexExpertise, setIndexExpertise] = useState<number>(0);
+  const [text, setText] = useState<string>("");
+  const [isDelete, setDelete] = useState<boolean>(false);
+  const [deltaTime, setDeltaTime] = useState<number>(200);
+
+  useEffect(() => {
+    const tick = setInterval(() => {
+      // textin tamamını al
+      const indexTotext = areasOfExpertise[indexExpertise];
+      // textin ekranda gösterilecek olan kısmı , silme modundaysa 1 eksilk , yazma modundaysa 1 ekle
+      const updateIntext = isDelete
+        ? indexTotext.substring(0, text.length - 1)
+        : indexTotext.substring(0, text.length + 1);
+      // texti setle
+      setText(updateIntext);
+      // silme modundaysa her işlem sonrası hızı arttır
+      if (isDelete) setDeltaTime((dtime) => dtime / 2);
+      // yazma modundaysa ve tüm tüm texti yazdıysa
+      if (!isDelete && updateIntext === indexTotext) {
+        setDelete(true);
+        setDeltaTime(2000);
+        // silme modundaysa ve tüm text silindiyse
+      } else if (isDelete && updateIntext === "") {
+        setDelete(false);
+        setDeltaTime(300);
+        // yazılacak yazının indexini belirle , mevcut indexi bir arttırarak tüm yazıların sayısı kadarının modunu al;
+        setIndexExpertise((index) => ++index % areasOfExpertise.length);
+      }
+    }, deltaTime);
+
+    // component didmount delete interval
+    return () => clearInterval(tick);
+  }, [indexExpertise, text]);
+
+  return (
+    <div className="col-span-5 bg-zinc-800 rounded-md ">
+      <div
+        id="code-head"
+        className="h-10 w-full bg-zinc-700 rounded-t-md flex items-center cursor-pointer"
+      >
+        <div className="flex items-center justify-between h-2/4 w-3/12 ml-1 bg-zinc-900 p-4 rounded-sm">
+          <span className="font-bold text-yellow-400">JS</span>{" "}
+          <span>Hello.js</span>{" "}
+          <span className="font-thin text-gray-200">x</span>
+        </div>
+      </div>
+      <div className="p-6 flex flex-wrap gap-5">
+        <code className="text-4xl font-bold text-gray-500">
+          // Selam,ben Serkan
+        </code>
+        <div className="text-4xl font-semibold text-green-600">
+          {"{"} <span className="border-r-4 border-zinc-600 ">{text}</span>{" "}
+          <span>Developer</span> {"}"}
+        </div>
       </div>
     </div>
-    <div className="p-10 flex flex-wrap gap-5">
-      <code className="text-4xl font-bold text-gray-500">
-        // Selam , ben Serkan
-      </code>
-      <div className="text-4xl font-semibold text-green-600">
-        {"{"} <span>FullStack</span> <span>Developer</span> {"}"}
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default CodeBlock;
