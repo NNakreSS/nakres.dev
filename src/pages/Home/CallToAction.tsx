dfe0758bb03f28f52af8af2c5fdfd80ae50c5cc1
NNakreSS <serkanatmaca15@gmail.com>
2024-03-09 13:25:52 +0300

    extract component

---
+++ src/pages/Home/CallToAction.tsx
@@ -0,0 +1,13 @@
+const CallToAction = () => (
+  <div className="col-span-5 flex flex-wrap items-start justify-center">
+    <h3 className="text-4xl font-bold">Bir fikriniz mi var ?</h3>
+    <p className="w-full text-center text-xl text-gray-200">
+      Hemen iletişime geçin, beraber gerçekleştirelim.
+    </p>
+    <button className="bg-blue-500 p-2 w-4/12 rounded-md text-2xl font-medium">
+      İletişim
+    </button>
+  </div>
+);
+
+export default CallToAction;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,10 @@
+import CallToAction from "./CallToAction";
 import CodeBlock from "./CodeBlock";
 
 const Home = () => (
   <main className="w-full grid grid-cols-10 gap-4 mt-10">
     <CodeBlock />
-    <div className="col-span-5 flex flex-wrap items-start justify-center">
-      <h3 className="text-4xl font-bold">Bir fikriniz mi var ?</h3>
-      <p className="w-full text-center text-xl text-gray-200">
-        Hemen iletişime geçin , beraber gerçekleştirelim.
-      </p>
-      <button className="bg-blue-500 p-2 w-4/12 rounded-md text-2xl font-medium">
-        İletişim
-      </button>
-    </div>
+    <CallToAction />
   </main>
 );
 
