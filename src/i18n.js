6204242e0ad606a5fa95a1d698fb4bc3c0960eda
NNakreSS <serkanatmaca15@gmail.com>
2024-03-12 23:55:14 +0300

    add language Detector

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,10 @@
 import i18n from "i18next";
 import BackEnd from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n.use(initReactI18next).use(BackEnd).init({
-  lng: "tr",
+i18n.use(initReactI18next).use(BackEnd).use(LanguageDetector).init({
+  fallbackLng: "en",
   debug: false,
 });
 
