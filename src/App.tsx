b548f5609429a19b8702450a15bf4e82382169c6
NNakreSS <serkanatmaca15@gmail.com>
2024-03-12 23:31:09 +0300

    remove suspense

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,28 @@ import Footer from "./components/Footer";
 import { useSelector } from "react-redux";
 import { themeSelector } from "./redux/slicers/themeSlice";
 import classNames from "classnames";
-import { Suspense } from "react";
 
 const App = () => {
   const { darkMode } = useSelector(themeSelector);
 
   return (
-    <Suspense fallback="loading">
-      <div
-        className={classNames(
-          "bg-background min-h-screen min-w-full flex flex-col transition duration-300 ease-linear",
-          {
-            light: !darkMode,
-            dark: darkMode,
-          }
-        )}
-      >
-        <Header />
-        <main className="flex-0 w-full flex flex-col gap-4 mt-10 gap-y-40 container mx-auto px-2 lg:px-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Suspense>
+    <div
+      className={classNames(
+        "bg-background min-h-screen min-w-full flex flex-col transition duration-300 ease-linear",
+        {
+          light: !darkMode,
+          dark: darkMode,
+        }
+      )}
+    >
+      <Header />
+      <main className="flex-0 w-full flex flex-col gap-4 mt-10 gap-y-40 container mx-auto px-2 lg:px-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 export default App;
