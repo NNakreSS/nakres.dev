93fc35a3f795c77e8b6c02e642a584136040746e
NNakreSS <serkanatmaca15@gmail.com>
2024-03-14 02:10:06 +0300

    Add project data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,21 +1,198 @@
-// projects api data
-
-export const projects = [
-  {
-    id: 0,
-    title: "aycan.dev",
-    category: "fivem",
-    description: "Source code of my portfolio web app.",
-    stack: ["React.js", "Next.js", "TypeScript", "Mantine"],
-    links: [
-      {
-        link: "https://github.com/aycanogut/portfolio-next",
-        id: 1,
+export const projects = {
+  web: [
+    {
+      title: "nakres.dev",
+      img: "https://media.discordapp.net/attachments/933994040851071026/1217604910196195378/312642255-55f83d74-f9d8-4eaf-a0e8-506224555dec.png?ex=6604a1f0&is=65f22cf0&hm=a409a0c70a32cca9e1b55989cc3963045905ad551b9d1769df68cfdafe38b1d6&=&format=webp&quality=lossless&width=687&height=335",
+      description: {
+        tr: "Portfolio web sitemin açık kaynak kodu.",
+        en: "Source code of my portfolio web app.",
       },
-      {
-        link: "https://aycan.dev",
-        id: 2,
+      stack: ["React.js", "TypeScript", "Redux", "Redux Toolkit", "Tailwind"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/my_website",
+        },
+        {
+          type: "demo",
+          link: "https://nakres.dev",
+        },
+      ],
+    },
+    {
+      title: "Turkish Checkers",
+      img: "https://media.discordapp.net/attachments/933994040851071026/1217606766314000488/302338331-ffbc5d35-7516-4d92-943f-982761bafd5e.png?ex=6604a3ab&is=65f22eab&hm=9950ccdc0240c9ab03f84f9d07c693d9bd6e191623e3447f89292a3bd1200256&=&format=webp&quality=lossless&width=687&height=340",
+      description: {
+        en: "A two-player Turkish checkers game playable on a single device.",
+        tr: "Türk daması oyununu tek bir cihaz üzerinde 2 kişinin oynayabileceği bir uygulama.",
       },
-    ],
-  },
-];
+      stack: ["React.js", "Tailwind", "Redux", "Redux Toolkit"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/turkish-checkers-game",
+        },
+        {
+          type: "demo",
+          link: "https://nnakress.github.io/turkish-checkers-game/",
+        },
+      ],
+    },
+    {
+      title: "Typing Speed",
+      img: "https://media.discordapp.net/attachments/933994040851071026/1217607142316576898/301333379-14094485-6ec6-43ac-8001-5f258d03c3d0.png?ex=6604a404&is=65f22f04&hm=254a65205a1b6dec108a356140c3afcfb77217604a287d0d0385e19c6c0e363f&=&format=webp&quality=lossless&width=1422&height=701",
+      description: {
+        tr: "10 Parmak klavye hız testi.",
+        en: "10 Finger keyboard speed test.",
+      },
+      stack: ["React.js", "Tailwind ", "Redux", "Redux Toolkit"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/react-typing-speed-test",
+        },
+        {
+          type: "demo",
+          link: "https://nnakress.github.io/react-typing-speed-test/",
+        },
+      ],
+    },
+    {
+      title: "Covid 19 Tracker",
+      img: "https://media.discordapp.net/attachments/933994040851071026/1217607291466158192/300976471-bbd46ced-1c16-47bd-aab0-30dc7bc51f01.png?ex=6604a428&is=65f22f28&hm=144d151ca8dd203c1b6d4dbcadd06ae14f606d1233e07903b60b9cae1b3e48ad&=&format=webp&quality=lossless&width=635&height=437",
+      description: {
+        tr: "Covid 19 Vakalarının global ve ülke bazlı durum takibini yapabildiğimiz bir uygulama.",
+        en: "An application where we can track the global and country-specific situation of Covid-19 cases.",
+      },
+      stack: ["React.js", "Tailwind ", "Redux", "Redux Toolkit"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/covid19tracker",
+        },
+        {
+          type: "demo",
+          link: "https://nnakress.github.io/covid19tracker/",
+        },
+      ],
+    },
+    {
+      title: "Note App",
+      img: "https://media.discordapp.net/attachments/933994040851071026/1217607417521766551/note.png?ex=6604a446&is=65f22f46&hm=bd4fa6824d70e10caef3453713e7c77ffc204e7f40fe64416b160a07ef32c3aa&=&format=webp&quality=lossless&width=687&height=386",
+      description: {
+        tr: "Next UI paketi ile tasarlanmış notlarım uygulaması.",
+        en: "A notes application designed with the Next UI package.",
+      },
+      stack: ["React.js", "Tailwind", "Next UI", "Redux", "Redux Toolkit"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/react-redux-noteApp",
+        },
+        {
+          type: "demo",
+          link: "https://nnakress.github.io/react-redux-noteApp/",
+        },
+      ],
+    },
+    {
+      title: "Memory Game",
+      img: "https://media.discordapp.net/attachments/933994040851071026/1217607546664124486/memory.png?ex=6604a465&is=65f22f65&hm=72fe13c10badca4adfc490285346d5416c2d80c367bf4baa2f202cc1d95c231f&=&format=webp&quality=lossless&width=687&height=338",
+      description: {
+        tr: "Kartları eşleştirdiğimiz bir bulmaca hafıza oyunu.",
+        en: "A memory puzzle game where we match cards.",
+      },
+      stack: ["React.js", "Tailwind", "Redux", "Redux Toolkit"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/memory-game",
+        },
+        {
+          type: "demo",
+          link: "https://nnakress.github.io/memory-game/",
+        },
+      ],
+    },
+  ],
+  mobile: [
+    {
+      title: "Space Voyage",
+      img: "https://private-user-images.githubusercontent.com/87872407/311020273-1098b591-8475-4181-9211-ebca7da10999.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTAzNzEwNDksIm5iZiI6MTcxMDM3MDc0OSwicGF0aCI6Ii84Nzg3MjQwNy8zMTEwMjAyNzMtMTA5OGI1OTEtODQ3NS00MTgxLTkyMTEtZWJjYTdkYTEwOTk5LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzEzVDIyNTkwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTMxNmUxNWVjMDVlZmEzZjYzNzcxY2RlYmVlMzM5ZjZlMTE0NzY2OWFiNWQ2MDdjMzM0NTJkMzM4ZWNmZDUxYTQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.lmkW9aW9djGNaVzkZEgveFAQ3x8cTafrZuYeYLAna_U",
+      description: {
+        tr: "Kodluyoruz ekibinin düzenlediği Hi-kod 2.0 mobil uygulama atölyesinde birincilik ödülünü kazandı!",
+        en: "It won the first prize in the Hi-code 2.0 mobile application workshop organized by the Kodluyoruz team!",
+      },
+      stack: ["Flutter", "Dart"],
+      links: [
+        {
+          type: "github",
+          link: "https://github.com/NNakreSS/space_voyage",
+        },
+        {
+          type: "youtube",
+          link: "https://www.youtube.com/watch?v=dG0xZdZJ6Eo",
+        },
+      ],
+    },
+  ],
+  fivem: [
+    {
+      title: "Bodycam",
+      img: "https://media.discordapp.net/attachments/933822022650310716/1187389242339491840/BANNER.png?ex=65fc38e7&is=65e9c3e7&hm=5ee1e05fcf7878a78b8a6353c4a6e87098f940613872edd38ed3720708e901ca&=&format=webp&quality=lossless&width=1246&height=701",
+      description: {
+        tr: "Polis rolleri için oyun içi bodycam kaydı , kaydı izlemek için bodycam tableti , özel kayıt bildirimleri , kayıt özelinde mesajlaşma ,canlı izleme ve dashcam gibi birçok özellik",
+        en: "In-game bodycam recording for police roles, a bodycam tablet for watching recordings, special recording notifications, messaging per recording, live streaming, and dashcam-like features.",
+      },
+      stack: ["HTML", "JS", "CSS", "LUA"],
+      links: [
+        {
+          type: "youtube",
+          link: "https://www.youtube.com/watch?v=tun29BbzXPs",
+        },
+        {
+          type: "store",
+          link: "https://nakres-development.tebex.io/package/4910502",
+        },
+      ],
+    },
+    {
+      title: "Taxi Job",
+      img: "https://media.discordapp.net/attachments/1043308209642946641/1043308821659000852/pp.PNG?ex=65fe160e&is=65eba10e&hm=e2d322cbbf8336afb0c5282e1d9d96e708aac77e60159f4f4f2cdaac09c3cf6d&=&format=webp&quality=lossless&width=870&height=701",
+      description: {
+        tr: "Sunucunuza taksi durakları ekleyin, oyuncular eklediğiniz duraklardan lisans alabilir ve istedikleri durağa kayıt olduktan sonra çalışmaya başlayabilirler.",
+        en: "Add taxi stops to your server, players can get a license from the stops you add and start work after they register for the desired stand.",
+      },
+      stack: ["HTML", "JS", "CSS", "LUA"],
+      links: [
+        {
+          type: "youtube",
+          link: "https://www.youtube.com/watch?v=YprY24oekGE",
+        },
+        {
+          type: "store",
+          link: "https://nakres-development.tebex.io/package/5386142",
+        },
+      ],
+    },
+    {
+      title: "Arm Wrestling",
+      img: "https://media.discordapp.net/attachments/1148988541846700163/1148989336667304006/image.png?ex=66042e35&is=65f1b935&hm=b2d3bf57d13bcff2121c25a19cd2e782fcdd9f2a0cb43cf69b2ec101487180fd&=&format=webp&quality=lossless&width=687&height=281",
+      description: {
+        tr: "NakreS Bilek Güreşi, sunucunuza heyecan verici bir bilek güreşi özelliği ekleyen premium bir FiveM betiğidir. Bu betik, oyuncularınıza benzersiz ve eğlenceli bir etkinlik sunmanıza olanak tanır.",
+        en: "NakreS Arm Wrestling  is a premium FiveM script that adds an exciting arm wrestling feature to your server. This script allows you to provide your players with a unique and entertaining activity.",
+      },
+      stack: ["HTML", "JS", "CSS", "LUA"],
+      links: [
+        {
+          type: "youtube",
+          link: "https://www.youtube.com/watch?v=gT6680wYk0k",
+        },
+        {
+          type: "store",
+          link: "https://nakres-development.tebex.io/package/5895153",
+        },
+      ],
+    },
+  ],
+};
