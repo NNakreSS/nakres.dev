36b781b6c328539e8fc14f97e6cd319ba1ebc2d5
NNakreSS <serkanatmaca15@gmail.com>
2024-03-15 18:32:41 +0300

    fix padding

--- src/pages/Articles/index.tsx
+++ src/pages/Articles/index.tsx
@@ -7,7 +7,7 @@ const Articles = () => (
     initial={{ opacity: 0, translateY: 100 }}
     animate={{ opacity: 1, translateY: 0 }}
   >
-    <h2 className="m-auto text-5xl text-text-main/60 p-20">Development...</h2>
+    <h2 className="m-auto text-5xl text-text-main/60">Development...</h2>
   </motion.div>
 );
 
