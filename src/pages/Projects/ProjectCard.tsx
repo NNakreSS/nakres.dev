81c790a70ab52fb1290800399f6ba9cbc77c2fcb
NNakreSS <serkanatmaca15@gmail.com>
2024-03-22 03:25:51 +0300

    fix image size

--- src/pages/Projects/ProjectCard.tsx
+++ src/pages/Projects/ProjectCard.tsx
@@ -34,6 +34,7 @@ const ProjectCard: React.FC<{
       transition={{ duration: 0.5 }}
       className="bg-card p-4 rounded-md shadow-inner shadow-black/30 flex flex-col justify-between"
     >
+      {/*  Header  */}
       <div className="flex items-center justify-between">
         <h4 className="text-lg font-semibold">{project.title}</h4>
         <div className="flex gap-2 text-2xl">
@@ -44,20 +45,23 @@ const ProjectCard: React.FC<{
           ))}
         </div>
       </div>
+
+      {/*  Main  */}
       <div className="flex flex-col md:flex-row gap-5 py-5 h-full">
         {project?.img && (
-          <div className="w-9/12 h-full rounded-md overflow-hidden shadow-md mx-auto">
-            <img
-              src={project?.img}
-              alt={project.title}
-              className="w-full h-full"
-            />
-          </div>
+          <img
+            src={project?.img}
+            alt={project.title}
+            className="object-cover md:h-52 w-6/12 rounded-md"
+          />
         )}
-        <p className="w-full text-start text-xl text-text-main/80">
+
+        <p className="w-6/12 text-start text-xl  text-text-main/80">
           {description[lang]}
         </p>
       </div>
+
+      {/*  Footer  */}
       <div className="flex flex-wrap items-center justify-start gap-x-1 gap-y-2 border-t-[1px] border-text-main/50 border-solid mt-2 pt-4">
         {project.stack.map((stack: any, _key: number) => (
           <span
