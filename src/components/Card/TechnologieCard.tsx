1f4cb7dac0e8434d7d3421e4fa30df606797a5e1
NNakreSS <serkanatmaca15@gmail.com>
2024-04-02 21:14:58 +0300

    lazy loading image

--- src/components/Card/TechnologieCard.tsx
+++ src/components/Card/TechnologieCard.tsx
@@ -22,7 +22,7 @@ const TechnologieCard: React.FC<ITechnologieCard> = (
         <span>{title}</span>
       </div>
       <div className="cardMain flex items-center justify-center h-4/6 group-hover:scale-125 duration-300 filter group-hover:drop-shadow-[0_0_5px_rgba(0,0,0,0.8)]">
-        <img src={imgSrc} alt={title} className="w-16 lg:w-28" />
+        <img src={imgSrc} alt={title} loading="lazy" className="w-16 lg:w-28" />
       </div>
       <div className="cardDescription flex items-center justify-center h-1/6">
         <p>{description}</p>
