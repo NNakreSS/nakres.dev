97a879ec73c3872e1c8de3b1bc55eaca9a4363e6
NNakreSS <serkanatmaca15@gmail.com>
2024-04-30 18:15:49 +0300

    project images refactor

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const projects = {
   web: [
     {
       title: "nakres.dev",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224765834162077716/image.png?ex=66312411&is=661eaf11&hm=a0717289e806815e316a298e8b29695b59f671b641da11fe602ce056774453c9&=&format=webp&quality=lossless&width=1432&height=701",
+      img: "projects_images/nakres_dev.webp",
       description: {
         tr: "Portfolio web sitemin açık kaynak kodu.",
         en: "Source code of my portfolio web app.",
@@ -21,7 +21,7 @@ export const projects = {
     },
     {
       title: "Url Shortener",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224765752368955442/273471984-afac82d4-7442-4e3e-8bab-1a772ccb7b13.png?ex=663123fe&is=661eaefe&hm=6822a56b9e00c72e60b83c147abe720c1f415ad3e963e0cb21ea7c194c91731e&=&format=webp&quality=lossless&width=961&height=701",
+      img: "projects_images/url_shortener.webp",
       description: {
         tr: "Bu proje, URL'leri kısaltabilen ve kısaltılan URL'leri yönetebilen bir PHP uygulamasını içerir. Ayrıca, bir üyelik sistemi ve yönetici paneli içerir.",
         en: "This project includes a PHP application that can shorten URLs and manage the shortened URLs. It also features a membership system and an admin panel.",
@@ -40,7 +40,7 @@ export const projects = {
     },
     {
       title: "Turkish Checkers",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224765910259208224/302338331-ffbc5d35-7516-4d92-943f-982761bafd5e.png?ex=66312424&is=661eaf24&hm=291493fd3368e0c5785e608f33bd1ee495b730e8aadc03d07994b409b5c3b764&=&format=webp&quality=lossless&width=687&height=340",
+      img: "projects_images/checkers.webp",
       description: {
         en: "A two-player Turkish checkers game playable on a single device.",
         tr: "Türk daması oyununu tek bir cihaz üzerinde 2 kişinin oynayabileceği bir uygulama.",
@@ -59,7 +59,7 @@ export const projects = {
     },
     {
       title: "Typing Speed Test",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224765964802068520/301333379-14094485-6ec6-43ac-8001-5f258d03c3d0.png?ex=66312431&is=661eaf31&hm=dc077506f1f4986430a04a4162ee89dc916534e8cf7a6d0e506de13378015503&=&format=webp&quality=lossless&width=687&height=338",
+      img: "projects_images/typing.webp",
       description: {
         tr: "10 Parmak klavye hız testi.",
         en: "10 Finger keyboard speed test.",
@@ -78,7 +78,7 @@ export const projects = {
     },
     {
       title: "Covid 19 Tracker",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224765995722342521/300976471-bbd46ced-1c16-47bd-aab0-30dc7bc51f01.png?ex=66312438&is=661eaf38&hm=76aa1c5908d2d9ddd07e98df6dc67d73d1097050c504d5104918be479272f1e3&=&format=webp&quality=lossless&width=635&height=437",
+      img: "projects_images/covid.webp",
       description: {
         tr: "Covid 19 Vakalarının global ve ülke bazlı durum takibini yapabildiğimiz bir uygulama.",
         en: "An application where we can track the global and country-specific situation of Covid-19 cases.",
@@ -97,7 +97,7 @@ export const projects = {
     },
     {
       title: "Note App",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224766044359622656/299321574-af55eb77-f986-4379-8616-4e676a621449.png?ex=66312444&is=661eaf44&hm=820001d38cbd8e3c536f2b89a1671cd69b4f561e73a1b0da02df3c5fe56b910d&=&format=webp&quality=lossless&width=687&height=386",
+      img: "projects_images/note_app.webp",
       description: {
         tr: "Next UI paketi ile tasarlanmış notlarım uygulaması.",
         en: "A notes application designed with the Next UI package.",
@@ -116,7 +116,7 @@ export const projects = {
     },
     {
       title: "Memory Game",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224766084926672956/300609979-ad97c2e3-0b3c-4e72-a31a-e1cbc604fd8e.png?ex=6631244d&is=661eaf4d&hm=a5f02826465ef6fe3b97c4bc5fd4262bd3454ce6315f80bcdcdfa24f4ae33a86&=&format=webp&quality=lossless&width=687&height=338",
+      img: "projects_images/memory_game.webp",
       description: {
         tr: "Kartları eşleştirdiğimiz bir bulmaca hafıza oyunu.",
         en: "A memory puzzle game where we match cards.",
@@ -137,7 +137,7 @@ export const projects = {
   mobile: [
     {
       title: "Space Voyage",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1226139523491299448/311020273-1098b591-8475-4181-9211-ebca7da10999.png?ex=6623ae6a&is=6611396a&hm=d5bbb9d07eda06013ecb96d4df0480170ec4b8a3d203569134af71db15777ccf&=&format=webp&quality=lossless&width=687&height=386",
+      img: "projects_images/space_voyage.webp",
       description: {
         tr: "Kodluyoruz ekibinin düzenlediği Hi-kod 2.0 mobil uygulama atölyesinde birincilik ödülünü kazandı!",
         en: "It won the first prize in the Hi-code 2.0 mobile application workshop organized by the Kodluyoruz team!",
@@ -158,7 +158,7 @@ export const projects = {
   fivem: [
     {
       title: "Bodycam",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1221193378952253490/BANNER.png?ex=662d5f76&is=661aea76&hm=5c2fc8b5d1d3a75b4a28d56110170a7ff0fb258dac0d306eeb74aaf80936c375&=&format=webp&quality=lossless&width=1246&height=701",
+      img: "projects_images/bodycam.webp",
       description: {
         tr: "Polis rolleri için oyun içi bodycam kaydı , kaydı izlemek için bodycam tableti , özel kayıt bildirimleri , kayıt özelinde mesajlaşma ,canlı izleme ve dashcam gibi birçok özellik",
         en: "In-game bodycam recording for police roles, a bodycam tablet for watching recordings, special recording notifications, messaging per recording, live streaming, and dashcam-like features.",
@@ -177,7 +177,7 @@ export const projects = {
     },
     {
       title: "Video Call",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224766181265641542/153680251-411a3b70-3318-484e-b7d2-060ca64f7c37.png?ex=66312464&is=661eaf64&hm=f60d087d278d0878e34ef3a36bb5c0101b37176453e93554de2f07f65ff150f3&=&format=webp&quality=lossless&width=687&height=370",
+      img: "projects_images/videocall.webp",
       description: {
         tr: "Telefonunuza entegre ederek oyun içerisinde kullanabileceğiniz bir görüntülü konuşma özelliği sunar. (qb-phone için default örnek mevcut)",
         en: "Offers a video call feature that you can integrate into your phone and use within the game. (Default example available for qb-phone)",
@@ -192,7 +192,7 @@ export const projects = {
     },
     {
       title: "Taxi Job",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1221194144752472145/pp.png?ex=662d602c&is=661aeb2c&hm=cdfce28937b9ba4db475fbbc17c6279e412f8013f42ad2b768804cd0e063f631&=&format=webp&quality=lossless&width=870&height=701",
+      img: "projects_images/taxi.webp",
       description: {
         tr: "Sunucunuza taksi durakları ekleyin, oyuncular eklediğiniz duraklardan lisans alabilir ve istedikleri durağa kayıt olduktan sonra çalışmaya başlayabilirler.",
         en: "Add taxi stops to your server, players can get a license from the stops you add and start work after they register for the desired stand.",
@@ -211,7 +211,7 @@ export const projects = {
     },
     {
       title: "Arm Wrestling",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224766345556525147/image.png?ex=6631248b&is=661eaf8b&hm=940d9b6ea29438261e4a23edbd0b3ac8a50eaf58eb672e18aec5831bc537a93e&=&format=webp&quality=lossless&width=687&height=281",
+      img: "projects_images/wrestling.webp",
       description: {
         tr: "NakreS Bilek Güreşi, sunucunuza heyecan verici bir bilek güreşi özelliği ekleyen premium bir FiveM betiğidir. Bu betik, oyuncularınıza benzersiz ve eğlenceli bir etkinlik sunmanıza olanak tanır.",
         en: "NakreS Arm Wrestling  is a premium FiveM script that adds an exciting arm wrestling feature to your server. This script allows you to provide your players with a unique and entertaining activity.",
@@ -230,7 +230,7 @@ export const projects = {
     },
     {
       title: "Zone Creator",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224767209604251799/303928942-f105b617-91f2-47fa-9fb5-749ce935d5bb.png?ex=66312559&is=661eb059&hm=1e79ffdc2d19d017eb4903eced57f36ef2a5409871b962e87e67bb5cf7b26ad3&=&format=webp&quality=lossless&width=687&height=386",
+      img: "projects_images/zone.webp",
       description: {
         tr: "Zone komut dosyasını kullanarak çoklu bölgelerin daha kolay oluşturulmasını sağlayan bir komut dosyası aracı.(PolyZone veya ox.lib zone).",
         en: "A script tool that facilitates easier creation of polyzones using the Zone script (PolyZone or ox.lib zone).",
@@ -245,7 +245,7 @@ export const projects = {
     },
     {
       title: "Lightbar",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1221194650619084882/unknown.png?ex=66237d65&is=66222be5&hm=d490447801a268ff35b4363b6efe59526803d6bf6df89ba1b6d55f75b88bc568&=&format=webp&quality=lossless&width=687&height=378",
+      img: "projects_images/lightbar.webp",
       description: {
         tr: "Araçlara çakar ekleyip kullanmanızı ve onları kaydetmenizi sağlayan gelişmiş lightbar scripti.",
         en: "Add lightbar to vehicles, manage them, manage lights and sounds.",
@@ -264,7 +264,7 @@ export const projects = {
     },
     {
       title: "Barbecue",
-      img: "https://media.discordapp.net/attachments/1221193222446125086/1224768023844360192/a76148b5ec931e293b3107516e5c5b08c9435050.png?ex=66234e5b&is=6621fcdb&hm=9a7d21c443965a9bdf4f5b7226d7d6e3455fab33008e486ff267cd6bdd2c948c&=&format=webp&quality=lossless&width=687&height=287",
+      img: "projects_images/barbecue.webp",
       description: {
         tr: "Bu script, FiveM platformunda kullanılmak üzere geliştirilmiş bir BBQ satış sistemini içerir. Bu sistem, oyuncuların barbekü objeleri oluşturmasına, işlerini yönetmelerine ve çevredeki NPC'lerden sipariş alarak yiyecek satmalarına olanak tanır.",
         en: "This script contains an advanced BBQ sales system developed for use on the FiveM platform. This system allows players to create barbecue objects, manage their business, and sell food by taking orders from nearby NPCs.",
