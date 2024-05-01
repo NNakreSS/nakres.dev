2533628bb9d0aab0f8c5479487151c50a0328958
NNakreSS <serkanatmaca15@gmail.com>
2024-05-01 13:30:22 +0300

    remove staggered children animations

--- src/pages/Articles/index.tsx
+++ src/pages/Articles/index.tsx
@@ -6,7 +6,6 @@ import {
 } from "../../redux/slicers/articlesSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { ArticleCard } from "../../components/Card";
-import StaggerChildren from "../../components/Animate/StaggerChildren";
 import { useTranslation } from "react-i18next";
 import Article from "../../interfaces/IArticle";
 import { Helmet } from "react-helmet";
@@ -29,23 +28,23 @@ const Articles = () => {
       <h1 className="text-text-main text-4xl font-semibold">
         {t("pages.articles").toUpperCase()}
       </h1>
-      <StaggerChildren className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 min-h-[45vh]">
-        {!loading ? (
-          articles?.map((article: Article, index: number) => (
+      {!loading && articles ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 min-h-[45vh]">
+          {articles?.map((article: Article, index: number) => (
             <ArticleCard
               key={index}
               article={article}
               className="p-5 flex flex-col justify-stretch gap-5"
             />
-          ))
-        ) : (
-          <div className="w-full col-span-full flex justify-center">
-            <div className="flex justify-center items-center">
-              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-200"></div>
-            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full col-span-full flex justify-center">
+          <div className="flex justify-center items-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-200"></div>
           </div>
-        )}
-      </StaggerChildren>
+        </div>
+      )}
     </section>
   );
 };
